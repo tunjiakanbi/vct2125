4716fe4e0b5cc7b186f6068cf280327ed6d0eff9
Tunji Akanbi <ac7236yr@e2381-as1750148.century.edu>
2020-01-16 11:06:21 -0600

    reworked script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var request = new XMLHttpRequest();
-request.open('GET','data.txt', false);
-request.send();
-console.log(request);
-document.getElementById('demo').innerHTML = request.responseText;
+for (var i = 0; i < 100; i++) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'data.txt', false);
+    request.send();
+
+    // document.getElementById('demo').innerHTML = request.responseText;
+    if (request.status === 200 && request.readyState === 4) {
+        console.log(request);
+        document.writeln(request.responseText);
+    }
+}
