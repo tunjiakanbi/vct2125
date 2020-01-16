72a749edb9609d55b91f557346e4a46a3702891a
Tunji Akanbi <ac7236yr@e2381-as1750148.century.edu>
2020-01-16 11:17:22 -0600

    minor fix

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 for (var i = 0; i < 100; i++) {
     var request = new XMLHttpRequest();
     request.open('GET', 'data.txt', false);
-    request.send();
-
-    // document.getElementById('demo').innerHTML = request.responseText;
-    if (request.status === 200) && (request.readyState === 4) {
-        console.log(request);
-        document.writeln(request.responseText);
+   
+    request.onreadyStateChange = function () {
+        // document.getElementById('demo').innerHTML = request.responseText;
+        if (request.status === 200 && request.readyState === 4) {
+            console.log(request);
+            document.writeln(request.responseText);
+        }
     }
+    request.send();
 }
