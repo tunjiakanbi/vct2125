523135b31a0815bc1508ce681614212f99324101
Tunji Akanbi <ac7236yr@e2405-a1520624.century.edu>
2020-01-16 16:02:56 -0600

    added for loop

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
+var request;
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.txt');
 
-    var request; 
-    if(window.XMLHttpRequest){
-        request = new XMLHttpRequest();
-    } else {
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    request.open('GET', 'data.txt');
-   
-    request.onreadystatechange = function () {
+request.onreadystatechange = function () {
 
-        if ((request.status === 200) && (request.readyState === 4)) {
-            // var modify = document.getElementById('demo');
-            var modify = document.getElementsByTagName('li');
-            // modify.innerHTML = request.responseText;
-            modify[2].innerHTML = request.responseText;
-   
+    if ((request.status === 200) && (request.readyState === 4)) {
+        // var modify = document.getElementById('demo');
+        var modify = document.getElementsByTagName('li');
+        // modify.innerHTML = request.responseText;
+        for (var i = 0; i < modify.length; i++) {
+            modify[i].innerHTML = request.responseText;
         }
     }
-    request.send();
+}
+request.send();
