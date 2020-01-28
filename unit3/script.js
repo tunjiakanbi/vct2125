e68914eb7ec4bc60bf087bc3bb76a3392c0cc2ac
Tunji Akanbi <ac7236yr@e2381-as1750148.century.edu>
2020-01-28 12:03:21 -0600

    updated Unit3 by adding jQuery

--- unit3/script.js
+++ unit3/script.js
@@ -1,22 +1,28 @@
-var request; 
-if(window.XMLHttpRequest){
-    request = new XMLHttpRequest();
-} else {
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
-request.open('GET', 'data.json');
-
-request.onreadystatechange = function () {
-
-    if ((request.status === 200) && (request.readyState === 4)) {
-        
-      var items = JSON.parse(request.responseText);
-    //   var output = '<ul>';
-    //   for (var i = 0; i < items.length; i++) {
-    //       output += '<li>' + items[i].firstChild.nodeValue + '</li>';
-    //   }
-    //   output += '</ul>';
-    //   document.getElementById('update').innerHTML = output;
+function loadAjax() {
+    var request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
     }
+    request.open('GET', 'data.json');
+
+    request.onreadystatechange = function () {
+
+        if ((request.status === 200) && (request.readyState === 4)) {
+
+            var items = JSON.parse(request.responseText);
+            console.log(items);
+            var output = '<ul>';
+            for (var key in items) {
+                output += '<li>' + items[key].artistname + '</li>';
+            }
+            //   for (var i = 0; i < items.length; i++) {
+            //       output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+            //   }
+            output += '</ul>';
+            document.getElementById('update').innerHTML = output;
+        }
+    }
+    request.send();
 }
-request.send();
