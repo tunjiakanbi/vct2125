3b6a69ca5b1e5241454b23ba687d3c43f0acba19
Tunji Akanbi <ac7236yr@e2381-as1750148.century.edu>
2020-03-23 13:54:59 -0500

    .

---
+++ unit8-react-demo/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
+const title = React.createElement(
+    'h1',
+    {id: 'title', className: 'header', style: style},
+    'Hello World'
+)
+ ReactDOM.render(
+     title,
+     document.getElementById('root')
+ )
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
